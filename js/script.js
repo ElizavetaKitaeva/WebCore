c3f365d9dacaeede7ff6e6bc2eb2058480492774
Eilla <eilla@student.21-school.ru>
2023-05-11 00:25:03 +0700

    1.5 slider

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-var init = false;
-
+let init = false;
+let checkClick = false;
 function swiperCard() {
-  if (window.innerWidth <= 768) {
-    if (!init) {
+  if (window.innerWidth < 768 && !init) {
       init = true;
       swiper = new Swiper(".swiper", {
         direction: "horizontal",
@@ -13,28 +12,64 @@ function swiperCard() {
           clickable: true,
         },
       });
-    }
-  } else if (init) {
+
+  } else if (init && window.innerWidth >= 768) {
     swiper.destroy();
     init = false;
-
   }
 }
+
 swiperCard();
 window.addEventListener("resize", swiperCard);
 
-let more = document.querySelector('.brands__button');
-var more_click = false;
-more.addEventListener('click', function() {
-  if (!more_click){
-    more_click = true;
-  document.querySelector('div .swiper').style.height = 'auto';
-  more.querySelector('p').textContent = 'Скрыть';
-  more.querySelector('.img').style.transform='rotate(180deg)';
-  } else {
-    more_click = false;
-    document.querySelector('div .swiper').style.height = '150px';
-    more.querySelector('p').textContent = 'Показать все';
-    more.querySelector('.img').style.transform='rotate(0deg)';
+function buttonMore(moreButton) {
+  let textButtonMore = moreButton.querySelector('p');
+  let imageButtonMore = moreButton.querySelector('.brands__button__image');
+  let swiperHeight = document.querySelector('div .swiper');
+  let cardView = document.querySelectorAll('.swiper-slide');
+
+  moreButton.addEventListener('click', function() {
+
+    switch (checkClick) {
+
+      case false:
+        checkClick = true;
+        swiperHeight.style.height = 'auto';
+        textButtonMore.textContent = 'Скрыть';
+        imageButtonMore.style.transform='rotate(180deg)';
+        cardView.forEach(element => {
+          element.style.display = 'block';
+        });
+        break;
+
+      case true:
+        checkClick = false;
+        swiperHeight.style.height = null;
+        textButtonMore.textContent = 'Показать все';
+        imageButtonMore.style.transform=null;
+        cardView.forEach(element => {
+          element.style.display = null;
+        });
+        break;
+
+      default:
+        checkClick = true;
+
+        break;
+    }
+
+  });
+}
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth < 768) {
+    let textButtonMore = document.querySelector('.brands__button__text');
+    let imageButtonMore = document.querySelector('.brands__button__image');
+    textButtonMore.textContent = 'Показать все';
+    imageButtonMore.style.transform=null;
   }
+  checkClick = false;
 });
+
+buttonMore(document.querySelector('.brands__button'));
+
